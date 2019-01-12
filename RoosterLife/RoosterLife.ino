c942de01c6ae40fac185b72fe39e7d5cf4dca601
Kent Fehribach <Kent.Fehribach@skykick.com>
2019-01-12 12:57:34 -0600

    What does it look like to make a external LED flash

--- RoosterLife/RoosterLife.ino
+++ RoosterLife/RoosterLife.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);  
+  pinMode(12, OUTPUT);  
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
