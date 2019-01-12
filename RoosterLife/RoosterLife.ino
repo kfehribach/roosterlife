ddfc17d08eb4398c10111ba49f6c5874d823671d
Kent Fehribach <Kent.Fehribach@skykick.com>
2019-01-12 13:48:53 -0600

    What does it look like to control low, normal, and high light scenarios using the light sensor

--- RoosterLife/RoosterLife.ino
+++ RoosterLife/RoosterLife.ino
@@ -1,12 +1,49 @@
+int sensorAnalogPin = A0;
+//int sensorDigitalPin = D0;
+
+int highLightPin = 12;
+int normalLightPin = 11;
+int lowLightPin = 10;
+
+int highLightThreshold = 75;
+int lowLightThreshold = 275;
+
 void setup() {
-  // put your setup code here, to run once:
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(12, OUTPUT);  
+  Serial.begin(9600);  
+  pinMode(highLightPin, OUTPUT);  
+  pinMode(normalLightPin, OUTPUT);  
+  pinMode(lowLightPin, OUTPUT);  
 }
 
 void loop() {
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  Serial.println(analogRead(sensorAnalogPin));
+  int currentLight = analogRead(sensorAnalogPin);
+  if(currentLight < highLightThreshold){
+    highLight();
+  }
+  else if(currentLight > lowLightThreshold){
+    lowLight();
+  }
+  else {
+    normalLight();
+  }
+  delay(100);                       // wait for a second
+}
+
+void highLight(){
+  digitalWrite(highLightPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(normalLightPin, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(lowLightPin, LOW);   // turn the LED on (HIGH is the voltage level)
+}
+
+void normalLight(){
+  digitalWrite(highLightPin, LOW);   
+  digitalWrite(normalLightPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(lowLightPin, LOW);   // turn the LED on (HIGH is the voltage level)
+}
+
+void lowLight(){
+  digitalWrite(highLightPin, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(normalLightPin, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(lowLightPin, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
